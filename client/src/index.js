73f06ccd547614a6ad4b421279de3899fdfd0c23
unknown <archivecoderzero@gmail.com>
2019-08-21 21:56:24 -0700

    added the ticking charts

--- client/src/index.js
+++ client/src/index.js
@@ -1,17 +1,27 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import { MuiThemeProvider, createMuiTheme } from "@material-ui/core/styles";
 import App from './App';
 import { BrowserRouter } from 'react-router-dom' //don't need to specify localhost url in axios http address
+
 
 //style
 import 'spectre.css/dist/spectre.min.css';
 import 'spectre.css/dist/spectre-icons.css';
 import './index.css';
 
+// LIVE CHARTS THEME
+const theme = createMuiTheme();
+// LIVE CHARTS THEME
+
+
 ReactDOM.render(
+	<MuiThemeProvider theme={theme}>
 	<BrowserRouter>
 		<App />
 	</BrowserRouter>,
+  </MuiThemeProvider>,
+
 	document.getElementById('root')
 )
