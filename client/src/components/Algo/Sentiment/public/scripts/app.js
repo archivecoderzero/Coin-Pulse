25d269215934da828a25acfe20ee26ff12b35b78
unknown <archivecoderzero@gmail.com>
2019-08-29 12:05:55 -0700

    do not merge

--- client/src/components/Algo/Sentiment/public/scripts/app.js
+++ client/src/components/Algo/Sentiment/public/scripts/app.js
@@ -39,18 +39,11 @@ $(document).ready(function() {
       var s = data[i].sentiment 
       var t = data[i].tweet;
 
-      let tweetScore = s.score ;
+      let tweetScore = s.score ? s.score:0;
       sentimentScore += 0 + tweetScore * parseInt((t.retweet_count + 1)) ;
-
-
-
     };
-
-
     $('.tweet-results').html(html);
     $('#sentimentScore').html(sentimentScore);
-
-
   }
 
   function oops(data) {
@@ -70,38 +63,3 @@ $(document).ready(function() {
 
 });
 
-
-// Simple JavaScript Templating
-// John Resig - http://ejohn.org/ - MIT Licensed
-(function() {
-  var cache = {};
-
-  this.tmpl = function tmpl(str, data) {
-    // Figure out if we're getting a template, or if we need to
-    // load the template - and be sure to cache the result.
-    var fn = !/\W/.test(str) ?
-      cache[str] = cache[str] ||
-      tmpl(document.getElementById(str).innerHTML) :
-
-    // Generate a reusable function that will serve as a template
-    // generator (and which will be cached).
-    new Function("obj",
-      "var p=[],print=function(){p.push.apply(p,arguments);};" +
-
-      // Introduce the data as local variables using with(){}
-      "with(obj){p.push('" +
-
-      // Convert the template into pure JavaScript
-      str
-      .replace(/[\r\t\n]/g, " ")
-      .split("{{").join("\t") // modified
-      .replace(/((^|\}\})[^\t]*)'/g, "$1\r") // modified
-      .replace(/\t=(.*?)}}/g, "',$1,'") // modified
-      .split("\t").join("');") 
-      .split("}}").join("p.push('") // modified
-      .split("\r").join("\\'") + "');}return p.join('');");
-
-    // Provide some basic currying to the user
-    return data ? fn(data) : fn;
-  };
-})();
