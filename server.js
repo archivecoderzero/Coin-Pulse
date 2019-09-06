e06f9b81760f37c28e5f4f00c0453c7f647e406b
unknown <archivecoderzero@gmail.com>
2019-09-05 20:58:21 -0700

    change server , socket IO , ..

--- server.js
+++ server.js
@@ -11,9 +11,9 @@ const MongoStore = require('connect-mongo')(session)
 const passport = require('./passport');
 var cors = require('cors');
 const app = express();
-const socketPort = process.env.socketPort || 8080;
+//const socketPort = process.env.socketPort || 8080;
 
-const PORT = process.env.PORT || 8000;
+const PORT = process.env.PORT || 3001;
 // Route requires
 const user = require('./routes')
 
@@ -21,9 +21,14 @@ const user = require('./routes')
 // // ## Routes and Serve Static Files
 // app.use(express.static(__dirname + './client/public'));
 
-app.get("/", function(req, res) {
+/* app.get("/", function (req, res) {
 	res.json(path.join(__dirname, "client/public/index.html"));
-  });
+}); */
+
+// Serve up static assets (usually on heroku)
+//if (process.env.NODE_ENV === "production") {
+	app.use(express.static("client/build"));
+//  }
 
 
 
@@ -62,46 +67,48 @@ app.use(user)
 
 
 //##SocketIO Server --> START
-var server = app.listen(socketPort);
-console.log('Socket IO ON port --> : %s', socketPort);
-var io = require('socket.io').listen(server);
-var connections = [];
-//Create socket.io Connection with Client
-//All Socket Listeners Here
-io.sockets.on('connection', function(socket) {
-	connections.push(socket);
-	console.log('%s Connected. %s sockets connected', socket.id, connections.length);
-	var prevSearch = false;
-	var twitterStream;
-	socket.on('search', function(payload) {
-	  console.log('Keyword: %s', payload.keyword);
-	  if(prevSearch) {
-		twitterStream.stop();
-		console.log(prevSearch);
-		console.log('stop stream');
-	  } else {
-		prevSearch = true;
-	  }
-	  twitterStream = Twitter.stream('statuses/filter', {language: 'en', track: payload.keyword});
-	  var lastTimestamp = Date.now(),
-		  speedLimiter = 800; //800ms
-	  //Turn on Twitter Stream
-	  twitterStream.on('tweet', function(tweet) {
-		if(tweet.timestamp_ms - lastTimestamp > speedLimiter) {
-		  lastTimestamp = Date.now();
-		  // Send Tweet Object to Client
-		  socket.emit('sendTweet', {tweet: sentiment.getSentiment(tweet, socket)});
-		}
-	  });
-	  socket.once('disconnect', function() {
-		connections.splice(connections.indexOf(socket), 1);
-		socket.disconnect();
-		twitterStream.stop();
-		console.log('Socket disconnected: %s sockets remaining', connections.length);
-	  });
-	});
-  }); //END io.sockets.on
-//##SocketIO Server --> END
+//var server = app.listen(socketPort);
+// console.log('Socket IO ON port --> : %s', socketPort);
+function startSocket(server) {
+	var io = require('socket.io')(server);
+	var connections = [];
+	//Create socket.io Connection with Client
+	//All Socket Listeners Here
+	io.sockets.on('connection', function (socket) {
+		connections.push(socket);
+		console.log('%s Connected. %s sockets connected', socket.id, connections.length);
+		var prevSearch = false;
+		var twitterStream;
+		socket.on('search', function (payload) {
+			console.log('Keyword: %s', payload.keyword);
+			if (prevSearch) {
+				twitterStream.stop();
+				console.log(prevSearch);
+				console.log('stop stream');
+			} else {
+				prevSearch = true;
+			}
+			twitterStream = Twitter.stream('statuses/filter', { language: 'en', track: payload.keyword });
+			var lastTimestamp = Date.now(),
+				speedLimiter = 800; //800ms
+			//Turn on Twitter Stream
+			twitterStream.on('tweet', function (tweet) {
+				if (tweet.timestamp_ms - lastTimestamp > speedLimiter) {
+					lastTimestamp = Date.now();
+					// Send Tweet Object to Client
+					socket.emit('sendTweet', { tweet: sentiment.getSentiment(tweet, socket) });
+				}
+			});
+			socket.once('disconnect', function () {
+				connections.splice(connections.indexOf(socket), 1);
+				socket.disconnect();
+				twitterStream.stop();
+				console.log('Socket disconnected: %s sockets remaining', connections.length);
+			});
+		});
+	}); //END io.sockets.on
+	//##SocketIO Server --> END
+}
 
 
 
@@ -112,22 +119,24 @@ io.sockets.on('connection', function(socket) {
 //27017 is the default mongoDB port
 const uri = 'mongodb://localhost:27017/coin-pulse' || process.env.MONGODB_URI;
 mongoose.connect(uri).then(
-    () => { 
-        /** ready to use. The `mongoose.connect()` promise resolves to undefined. */ 
-        console.log('Connected to Mongo');
-    },
-    err => {
-         /** handle initial connection error */ 
-         console.log('error connecting to Mongo: ')
-         console.log(err);
-        }
-  );
+	() => {
+		/** ready to use. The `mongoose.connect()` promise resolves to undefined. */
+		console.log('Connected to Mongo');
+	},
+	err => {
+		/** handle initial connection error */
+		console.log('error connecting to Mongo: ')
+		console.log(err);
+	}
+);
 
 
 
 // Starting Server 
-app.listen(PORT, () => {
+const server = app.listen(PORT, () => {
 	console.log(`App listening on PORT: ${PORT}`)
 })
+
+startSocket(server);
 
 module.exports = app;
