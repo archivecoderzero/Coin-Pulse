41a9b8dfe6b43bba1e6c1db6d1450487189786fd
Tsige <zigkebede@gmaial.com>
2019-09-07 00:25:16 -0700

    landing/Signup page..

--- client/src/pages/Index.js
+++ client/src/pages/Index.js
@@ -13,19 +13,13 @@ import SignupCard from "../components/IndexComponents/SignupCard";
 // INDEX COMPONENT ----> END
 
 import { Col, Row, Container } from "../components/Grid";
-import './index.css';
-
+import "./index.css";
 
 class Index extends Component {
+  componentDidMount() {}
 
-  componentDidMount() {
-    
-  }  
-  
   render() {
     return (
-
-
       <Container fluid>
         {/* MAIN CARD START -----------------------> */}
         <Row>
@@ -35,16 +29,23 @@ class Index extends Component {
         </Row>
         {/* MAIN CARD END -----------------------> */}
 
-
         {/* ABOUT CARD START -----------------------> */}
         <Row>
-          
           <div className="block" id="about">
             <Container>
               <Row>
                 <Col size="6">
                   <p className="aboutTitle">Satoshi-Pulse</p>
-                  <h4>The main use for this application is to tract the sentiment of the market based on current trending news . Trend analysis application is one of the best tool beginner traders are looking for , although sentiment analysis tools are available , it is quite expensive and complicated for beginners and hobbyist . This application will solve the complexity of sentiment analysis by making it as simple as having 3 inputs and a click of a button .</h4>
+                  <h4>
+                    The main use for this application is to tract the sentiment
+                    of the market based on current trending news . Trend
+                    analysis application is one of the best tool beginner
+                    traders are looking for , although sentiment analysis tools
+                    are available , it is quite expensive and complicated for
+                    beginners and hobbyist . This application will solve the
+                    complexity of sentiment analysis by making it as simple as
+                    having 3 inputs and a click of a button .
+                  </h4>
                 </Col>
                 <Col size="6">
                   <img></img>
@@ -60,27 +61,27 @@ class Index extends Component {
           <div className="block" id="team">
             <h1>Meet the Team </h1>
             <Row>
-            <Col size="4">
-              <div >
-                <img className="dev" src={require('./Image/Raphael.png')} />
-              </div>
-              <h4 style={{color: "palevioletred"}}>Raphael</h4>
-              <h6 style={{color: "pink"}}>Full Stack Web Developer</h6>
-            </Col>
-            <Col size="4">
-              <div>
-                <img className="dev" src={require('./Image/Zia.png')} />
+              <Col size="4">
+                <div>
+                  <img className="dev" src={require("./Image/Raphael.png")} />
                 </div>
-              <h4 style={{color: "palevioletred"}}>Zia</h4>
-              <h6 style={{color: "pink"}}>Full Stack Web Developer</h6>
-            </Col>
-            <Col size="4">
-              <div >
-              <img className="dev" src={require('./Image/Alex.png')} />
-              </div>
-              <h4 style={{color: "palevioletred"}}>Alex</h4>
-              <h6 style={{color: "pink"}}>Full Stack Web Developer</h6>
-            </Col>
+                <h4 style={{ color: "palevioletred" }}>Raphael</h4>
+                <h6 style={{ color: "pink" }}>Full Stack Web Developer</h6>
+              </Col>
+              <Col size="4">
+                <div>
+                  <img className="dev" src={require("./Image/Zia.png")} />
+                </div>
+                <h4 style={{ color: "palevioletred" }}>Zia</h4>
+                <h6 style={{ color: "pink" }}>Full Stack Web Developer</h6>
+              </Col>
+              <Col size="4">
+                <div>
+                  <img className="dev" src={require("./Image/Alex.png")} />
+                </div>
+                <h4 style={{ color: "palevioletred" }}>Alex</h4>
+                <h6 style={{ color: "pink" }}>Full Stack Web Developer</h6>
+              </Col>
             </Row>
           </div>
         </Row>
@@ -90,30 +91,55 @@ class Index extends Component {
         <Row>
           <div className="block" id="policy">
             <Container>
-            <p id="policyTitle">Corporate Policy</p>
-            <p id="policyText">We are committed to complying with all U.S. regulations that help prevent, detect and remediate unlawful behavior by customers and virtual currency developers when using the Satoshi-Pulse trading platform or any of the company’s other services. Satoshi-Pulse is not a regulated exchange under U.S. securities laws.
-            </p>
+              <p id="policyTitle">Corporate Policy</p>
+              <p id="policyText">
+                We are committed to complying with all U.S. regulations that
+                help prevent, detect and remediate unlawful behavior by
+                customers and virtual currency developers when using the
+                Satoshi-Pulse trading platform or any of the company’s other
+                services. Satoshi-Pulse is not a regulated exchange under U.S.
+                securities laws.
+              </p>
             </Container>
           </div>
         </Row>
         <Row>
           <div className="block" id="signup">
             <Container>
-              <div className="width">
-                <div className="signupLeft">
-                  Submit your Token for Listing
-                </div>
-                <div className="signupRight">
-                  <button>Start Listing Process</button>
-                </div>
+              <div class="formGroup">
+                <label for="input" style={{ color: "white" }}>
+                  Sign Up
+                </label>
+                <input
+                  type="text"
+                  class="form-control"
+                  id="inputUsername"
+                  aria-describedby="userName"
+                  placeholder="User Name"
+                />
               </div>
+              <br />
+              <div class="formGroup">
+                <input
+                  type="password"
+                  class="form-control"
+                  id="exampleInputPassword1"
+                  placeholder="Password"
+                />
+              </div>
+              <br />
+              <button type="submit" id="submitBtn" class="btn btn-primary">
+                Submit
+              </button>
+              <hr/>
+              <small id="userHelp" class="form-text text-muted">As a verified user of this site you will be able to trade in markets offered by Satoshi-Pulse</small>
             </Container>
           </div>
         </Row>
         {/* DEVS CARD START -----------------------> */}
-
       </Container>
-    )
-}}
+    );
+  }
+}
 
 export default Index;
